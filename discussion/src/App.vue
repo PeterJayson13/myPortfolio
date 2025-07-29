2413d3bbcbb444e73aee5dbf969dd6925d49a7c1
peterbongabong7 <peterbongabong7@gmail.com>
2025-07-29 08:47:04 +0800

    first commit

---
+++ discussion/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Tools from './components/Tools.vue'
+import Contacts from './components/Contacts.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class= "container">
+    <About />
+  </div>
+  <Projects />
+  <Tools />
+  <Contacts />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
